926798ba272f039199bb06e3bb39412185e4fc54
Robin Lindqvist <robinlindqvist@live.se>
2025-04-23 14:06:52 +0200

    Fix: uppdaterar vite.config och lägger till 404.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ghPages from "vite-plugin-gh-pages"; // 👈 Lägg till denna import
 
 export default defineConfig({
-  plugins: [react(), ghPages()], // 👈 Lägg till plugin här
-  base: "/todo-game/", // 👈 superviktigt! samma namn som på GitHub
+  plugins: [react()],
+  base: "/todo-game/", // detta är rätt och ska vara kvar
 });
