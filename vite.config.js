eaeec39fddf651f207f1658129489f55d5666597
Robin Lindqvist <robinlindqvist@live.se>
2025-04-23 14:17:10 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-game/", // detta är rätt och ska vara kvar
+  base: "/todo-game/", // OBS! exakt repo-namn här
 });
